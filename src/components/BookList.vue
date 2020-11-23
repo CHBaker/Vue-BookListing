b6257a9865764de981224d329330bbe74d26b78e
charles baker <Charles.Baker@siriusxm.com>
2020-11-23 14:43:37 -0700

    FEAT: add books iteration

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -3,6 +3,11 @@
     <h1>
       {{ title }}
     </h1>
+    <ul>
+      <li v-for="(book, index) in books" :key="index">
+        {{ book.title }}: {{ book.author }}
+      </li>
+    </ul>
   </div>
 </template>
 <script>
@@ -10,7 +15,12 @@ export default {
   name: 'BookList',
   data() {
     return {
-      title: 'All Books'
+      title: 'All Books',
+      books: [
+        {title: 'Anatomy of State', author: 'Murray M. Rothbard'},
+        {title: 'Atlas Shrugged', author: 'Ayn Rand'},
+        {title: 'A Short History of Man', author: 'Hans Herman Hoppe'},
+      ]
     }
   },
 }
@@ -18,5 +28,11 @@ export default {
 <style scoped>
 h1, h2 {
   font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+  display: block;
+  margin: 0 10px;
 }
 </style>
