492c76de04b0333360408128c2b57acc3f5ffde5
charles baker <Charles.Baker@siriusxm.com>
2020-11-23 16:19:03 -0700

    FEAT: add form

---
+++ src/components/BookForm.vue
@@ -0,0 +1,27 @@
+<template>
+  <form class="" action="#" method="post">
+    <input v-model="bookTitle" type="text" name="title" value="" placeholder="Book Title">
+    <input v-model="bookAuthor" type="text" name="author" value="" placeholder="Book Author">
+    <button @click.prevent="bookSubmit()" type="submit" name="button">Add Book</button>
+  </form>
+</template>
+<script>
+export default {
+  name: 'BookForm',
+  props: [ 'books' ],
+  methods: {
+    bookSubmit (bookTitle, bookAuthor) {
+
+    }
+  },
+  data() {
+    return {
+      bookTitle: '',
+      bookAuthor: ''
+    }
+  },
+}
+</script>
+<style scoped>
+
+</style>
